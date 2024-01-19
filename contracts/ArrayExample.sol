6739ede6c5ac68b3efe55d79649a155b55be1172
Gino <ginowine1@gmail.com>
2024-01-19 22:24:52 +0100

    loop through an array

--- contracts/ArrayExample.sol
+++ contracts/ArrayExample.sol
@@ -6,6 +6,12 @@ contract ArrayExample {
     uint[] public arr2 = [1, 2, 3];
     uint[10] public arr3;
 
+    constructor (){
+        for (uint i = 0; i < 500; ++i) {
+            arr.push(i);
+        }
+    }
+
     function get(uint i) public view returns(uint){
         return arr[i];
     }
@@ -26,5 +32,12 @@ contract ArrayExample {
     }
     function examples() external pure {
         uint[] memory a = new uint[](5);
+    }
+
+    function loopArray() public {
+        uint arrLen = arr.length;
+        for(uint i = 0; i < arrLen; ++i){
+            arr[i] = arr[i] +1;
+        }
     }
 }
