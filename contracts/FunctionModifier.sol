227287bfa63a79e210db9d329624d2c397001f38
Gino <ginowine1@gmail.com>
2024-01-22 13:53:56 +0100

    create and use modifiers

---
+++ contracts/FunctionModifier.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract FunctionModifier {
+    address public owner;
+    uint public x = 10;
+    bool public locked;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier validAddress (address _addr){
+        require(_addr != address(0), "Not a valid address");
+        _;
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner validAddress(_newOwner){
+        owner = _newOwner;
+    }
+}
