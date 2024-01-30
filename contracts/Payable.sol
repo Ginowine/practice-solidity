e526a48b7c53aa22cc74b940d6e4bd560f4a6cd3
Gino <ginowine1@gmail.com>
2024-01-30 06:35:57 +0100

    update contract

--- contracts/Payable.sol
+++ contracts/Payable.sol
@@ -3,25 +3,30 @@ pragma solidity 0.8.19;
 
 contract Payable {
     address payable public owner;
+    uint256 public balance;
 
     constructor()payable {
         owner = payable(msg.sender);
     }
 
-    function deposit () public payable {}
+    function deposit () public payable {
+        balance += msg.value;
+    }
 
+    // calling this function will throw an error since it is not payable
     function notPayable() public{}
 
     function withdaw() public {
-        uint amount = address (this).balance;
+        require(msg.sender == owner, "Only owner can call this function");
+        uint256 amount = address (this).balance;
 
-        (bool success, ) = owner.call{value: amount}("");
+        (bool success, ) = payable (msg.sender).call{value: amount}("");
         require(success, "Failed to send Ether");
     }
 
-    function transfer(address payable _to, uint _amount) public {
-    (bool success, ) = _to.call{value: _amount}("");
-    require(success, "Failed to send Ether");
+    function transfer (address payable _to, uint256 _amount) public {
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Transfer failed");
     }
 
 }
